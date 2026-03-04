d7a1af535797037dd0822fd01a5622875e926cfe
gempitadini <gempitadini05@gmail.com>
2026-03-04 20:05:29 +0700

    menambahkan fungsi pengecekan bilangan prima

--- week3/src/prima.dart
+++ week3/src/prima.dart
@@ -3,4 +3,20 @@ import 'dart:io';
 void main() {
   stdout.write("Masukkan angka: ");
   int angka = int.parse(stdin.readLineSync()!);
+
+  if (isPrima(angka)) {
+    print("$angka adalah bilangan prima");
+  } else {
+    print("$angka bukan bilangan prima");
+  }
+}
+
+bool isPrima(int n) {
+  if (n <= 1) return false;
+
+  for (int i = 2; i < n; i++) {
+    if (n % i == 0) return false;
+  }
+
+  return true;
 }
